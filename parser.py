408fd3098ead1f49ae0323b89f88488aaaef9e06
herypank <60302361+herypank@users.noreply.github.com>
2020-08-04 00:22:50 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,5 +7,5 @@ url = 'http://tululu.org/b1/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 title_tag = soup.find('h1')
-print('Название книги: ' + title_tag.text[:title_tag.text.find(':')]+'\n' + 'Автор:' + title_tag.text[4 + title_tag.text.find(':'):])
-
+f=(title_tag.text.strip())
+print(f.split('\xa0 :: \xa0'))
