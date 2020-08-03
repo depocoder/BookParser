4c7c74242c90d53a18c13624fbf5a3fe825449b9
herypank <60302361+herypank@users.noreply.github.com>
2020-08-03 17:26:51 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,14 +1,14 @@
+import os
 import requests
+from pathlib import Path
 
+PATCH = r"C:\Users\lysak.m\Documents\py\study_prog\Many_projects\Bitly-CMD\books"
 
+Path(PATCH).mkdir(parents=True, exist_ok=True)
 for id in range(1,11):
     url = f'http://tululu.org/txt.php?id={id}'
     response = requests.get(url)
     if response.ok:
         filename = f"id{id}.txt"
-        with open(filename, "w") as file:
+        with open(f"{PATCH}/{filename}", "w") as file:
             file.write(response.text )
-"""url = "http://bit.ly/18SuUzJ"
-r = requests.get(url, allow_redirects=False)
-r.status_code
-r.headers['Location']"""
