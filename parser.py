eeff9aef651056c2a82c1bb8596d21e08640a766
herypank <60302361+herypank@users.noreply.github.com>
2020-08-03 23:59:43 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,14 +1,11 @@
 import os
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
 
-PATCH = r"C:\Users\lysak.m\Documents\py\study_prog\Many_projects\Bitly-CMD\books"
+url = 'http://tululu.org/b1/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+title_tag = soup.find('h1')
+print('Название книги: ' + title_tag.text[:title_tag.text.find(':')]+'\n' + 'Автор:' + title_tag.text[4 + title_tag.text.find(':'):])
 
-Path(PATCH).mkdir(parents=True, exist_ok=True)
-for id in range(1,11):
-    url = f'http://tululu.org/txt.php?id={id}'
-    response = requests.get(url)
-    if response.ok:
-        filename = f"id{id}.txt"
-        with open(f"{PATCH}/{filename}", "w") as file:
-            file.write(response.text)
