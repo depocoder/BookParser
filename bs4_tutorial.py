20e08e65897e8df48c6daee3118e244e393c813f
herypank <60302361+herypank@users.noreply.github.com>
2020-08-03 23:21:51 +0300

    Update bs4_tutorial.py

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -6,5 +6,7 @@ response = requests.get(url)
 response.raise_for_status()
 soup = BeautifulSoup(response.text, 'lxml')
 title_tag = soup.find('main').find('header').find('h1')
-img = soup.find('img', class_='attachment-post-image') # поиск через классы
-print(title_tag.text, img)
+img_src = soup.find('img', class_='attachment-post-image')['src'] # поиск через классы
+post_body = soup.find('div', class_='entry-content') # поиск через классы
+
+print(title_tag.text + '\n' + img_src + '\n' + post_body.text)
