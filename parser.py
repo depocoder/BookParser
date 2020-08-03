3ff05353ac5f099a35bdea73cd56888129a54d2c
herypank <60302361+herypank@users.noreply.github.com>
2020-08-03 16:53:55 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,8 +1,14 @@
 import requests
 
-url = 'http://tululu.org/txt.php?id=32168'
-response = requests.get(url)
 
-filename = "id1.txt"
-with open(filename, "w") as file:
-    file.write(response.text )
+for id in range(1,11):
+    url = f'http://tululu.org/txt.php?id={id}'
+    response = requests.get(url)
+    if response.ok:
+        filename = f"id{id}.txt"
+        with open(filename, "w") as file:
+            file.write(response.text )
+"""url = "http://bit.ly/18SuUzJ"
+r = requests.get(url, allow_redirects=False)
+r.status_code
+r.headers['Location']"""
