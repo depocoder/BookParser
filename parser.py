53866048312c2924c6d9f58b135556ed6beea763
herypank <60302361+herypank@users.noreply.github.com>
2020-08-04 14:46:56 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,12 +2,31 @@ import os
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
-url = 'http://tululu.org/b1/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-title_tag = soup.find('h1')
-f=(title_tag.text.strip())
-book = f.split(' \xa0 :: \xa0 ')
-print('Название книги:', book[0])
-print('Автор книги:', book[1])
+
+def parsing_page():
+    url_book = f'http://tululu.org/b{id}/'
+    response_book = requests.get(url_book, allow_redirects=False)
+    soup = BeautifulSoup(response_book.text, 'lxml')
+    title_tag = soup.find('h1')
+    parse_book = (title_tag.text.strip())
+    parse_book = parse_book.split(' \xa0 :: \xa0 ')
+    title_book = sanitize_filename(book[0])
+    author = sanitize_filename(book[1])
+
+    return title_book, '--', author
+
+
+if __name__ == '__main__':
+    PATCH = r"C:\Users\lysak.m\Documents\py\study_prog\Many_projects\BookParser\books"
+    Path(PATCH).mkdir(parents=True, exist_ok=True)
+
+    for id in range(1,10):
+        url_download = f'http://tululu.org/txt.php?id={id}'
+        response_download = requests.get(url_download, allow_redirects=False)
+        if not response_download.status_code == 302:
+            filename = f"{parsing_page()}.txt"
+            folder = os.path.join(PATCH, filename)
+            with open(folder, "w") as file:
+                file.write(response_download.text)
