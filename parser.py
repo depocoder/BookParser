f27db847734446467838719ee29c7adefa4ce8f5
herypank <60302361+herypank@users.noreply.github.com>
2020-08-04 14:54:33 +0300

    download books with titles

--- parser.py
+++ parser.py
@@ -12,21 +12,21 @@ def parsing_page():
     title_tag = soup.find('h1')
     parse_book = (title_tag.text.strip())
     parse_book = parse_book.split(' \xa0 :: \xa0 ')
-    title_book = sanitize_filename(book[0])
-    author = sanitize_filename(book[1])
+    title_book = sanitize_filename(parse_book[0])
+    author = sanitize_filename(parse_book[1])
 
-    return title_book, '--', author
+    return title_book + ' -- ' +  author
 
 
 if __name__ == '__main__':
     PATCH = r"C:\Users\lysak.m\Documents\py\study_prog\Many_projects\BookParser\books"
     Path(PATCH).mkdir(parents=True, exist_ok=True)
 
-    for id in range(1,10):
+    for id in range(1,11):
         url_download = f'http://tululu.org/txt.php?id={id}'
         response_download = requests.get(url_download, allow_redirects=False)
         if not response_download.status_code == 302:
-            filename = f"{parsing_page()}.txt"
+            filename = f"{id}. {parsing_page()}.txt"
             folder = os.path.join(PATCH, filename)
             with open(folder, "w") as file:
                 file.write(response_download.text)
