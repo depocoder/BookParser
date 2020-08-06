acaa19bd660378728d0e8a4cb365cd42f48d4639
herypank <60302361+herypank@users.noreply.github.com>
2020-08-06 11:01:56 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,19 +6,7 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
 import json
 
-def parsing_text(id,url_book):
-
-def parsing_text(id):
-    """Функция для парсинга названия книг с сайта http://tululu.org.
-
-    Args:
-        url_book (str): Cсылка на книгу которую парсим.
-        parse_book (list): (0)Название книги, (1)Автор.
-
-    Returns:
-        str: Путь до файла, куда сохранён текст.
-    """
-    url_book = f'http://tululu.org/b{id}/'
+def parsing_text(url_book):
     response = requests.get(url_book)
     soup = BeautifulSoup(response.text, 'lxml')
     header = soup.select_one("#content")
@@ -26,19 +14,9 @@ def parsing_text(id):
     parse_book = (title_tag).text.split(' \xa0 :: \xa0 ')
     author,title = parse_book
 
-    return author + ' -- ' +  title
+    return sanitize_filename(author) + ' -- ' +  sanitize_filename(title)
 
-def parsing_comments(id):
-    """Функция для парсинга комментариев книг с сайта http://tululu.org.
-
-    Args:
-        url_book (str): Cсылка на книгу которую парсим.
-        parse_book (list): (0)Название книги, (1)Автор.
-
-    Returns:
-        str: Путь до файла, куда сохранён текст.
-    """
-    url_book = f'http://tululu.org/b{id}/'
+def parsing_comments(url_book):
     response = requests.get(url_book)
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.select(".texts[style='margin:0;padding:0 10px;'] > .black")
@@ -47,17 +25,7 @@ def parsing_comments(id):
         comments.append(comment.text)
     return comments
 
-def parsing_genres(id):
-    """Функция для парсинга комментариев книг с сайта http://tululu.org.
-
-    Args:
-        url_book (str): Cсылка на книгу которую парсим.
-        parse_book (list): (0)Название книги, (1)Автор.
-
-    Returns:
-        str: Путь до файла, куда сохранён текст.
-    """
-    url_book = f'http://tululu.org/b{id}/'
+def parsing_genres(url_book):
     response = requests.get(url_book)
     soup = BeautifulSoup(response.text, 'lxml')
     genres_p = soup.find('span', class_ = 'd_book').find_all('a')
@@ -68,24 +36,13 @@ def parsing_genres(id):
 
 
 
-def parsing_image(id):
-    """Функция для парсинга картинок книг с сайта http://tululu.org.
-
-    Args:
-        url_book (str): Cсылка на книгу которую парсим.
-        parse_book (list): (0)Название книги, (1)Автор.
-
-    Returns:
-        str: Путь до файла, куда сохранён текст.
-    """
-    url_book = f'http://tululu.org/b{id}/'
+def parsing_image(url_book):
     response = requests.get(url_book)
     soup = BeautifulSoup(response.text, 'lxml')
     img_src = soup.find('div', class_ = 'bookimage').find('img')['src']
     return urljoin('http://tululu.org', img_src)
 
 def download_img(PATCH_IMG,url_img):
-
     response = requests.get(url_img, allow_redirects=False)
     filename = f"{url_img.split('/')[-1]}"
     folder = os.path.join(PATCH_IMG, filename)
@@ -96,10 +53,22 @@ def download_book(url_book,id):
     id_download = url_book[url_book.find('/b')+2:-1] #так как для закачки книги ссылка совсем другая нужен id
     url_download = f'http://tululu.org/txt.php?id={id_download}'
     response = requests.get(url_download, allow_redirects=False)
-    filename = f"{id+1}-я книга. {parsing_text(id_download,url_book)}.txt"
+    filename = f"{id+1}-я книга. {parsing_text(url_book)}.txt"
     folder = os.path.join(PATCH_BOOKS, filename)
     with open(folder, "w", encoding='utf-8') as file:
         return file.write(response.text)
+
+def parsing_url():
+    genre_links = []
+    for id in range(1,5):
+        url_book = f'http://tululu.org/l55/{id}'
+        response = requests.get(url_book)
+        soup = BeautifulSoup(response.text, 'lxml')
+        link_parse = soup.find_all('table', class_= 'd_book')
+        for link in link_parse:
+            link = link.find('a')['href']
+            genre_links.append(urljoin('http://tululu.org',link))
+    return genre_links
 
 if __name__ == '__main__':
     PATCH_BOOKS = r"C:\Users\lysak.m\Documents\py\study_prog\Many_projects\BookParser\books"
@@ -110,10 +79,10 @@ if __name__ == '__main__':
     list = []
     for id in range(10):
         url_book = urls[id]
-        url_img = parsing_image(id,url_book)
-        book_info = parsing_text(id,url_book).split(' -- ')
-        comments = parsing_comments(id,url_book)
-        genres = parsing_genres(id,url_book)
+        url_img = parsing_image(url_book)
+        book_info = parsing_text(url_book).split(' -- ')
+        comments = parsing_comments(url_book)
+        genres = parsing_genres(url_book)
         url_src = os.path.join('images',url_img.split('/')[-1])
         book_path = os.path.join('books',book_info[0] + '.txt')
         fantosy_book = {
