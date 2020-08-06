d7ae619b6c93cf3586e3d0d9c592faa91af33abb
herypank <60302361+herypank@users.noreply.github.com>
2020-08-06 11:23:23 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,39 +6,28 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
 import json
 
-def parsing_text(url_book):
-    response = requests.get(url_book)
-    soup = BeautifulSoup(response.text, 'lxml')
+def parsing_text(soup):
     header = soup.select_one("#content")
     title_tag = header.h1
     parse_book = (title_tag).text.split(' \xa0 :: \xa0 ')
     author,title = parse_book
-
     return sanitize_filename(author) + ' -- ' +  sanitize_filename(title)
 
-def parsing_comments(url_book):
-    response = requests.get(url_book)
-    soup = BeautifulSoup(response.text, 'lxml')
+def parsing_comments(soup):
     title_tag = soup.select(".texts[style='margin:0;padding:0 10px;'] > .black")
     comments = []
     for comment in title_tag:
         comments.append(comment.text)
     return comments
 
-def parsing_genres(url_book):
-    response = requests.get(url_book)
-    soup = BeautifulSoup(response.text, 'lxml')
+def parsing_genres(soup):
     genres_p = soup.find('span', class_ = 'd_book').find_all('a')
     genres = []
     for genre in genres_p:
         genres.append(genre.text)
     return genres
 
-
-
-def parsing_image(url_book):
-    response = requests.get(url_book)
-    soup = BeautifulSoup(response.text, 'lxml')
+def parsing_image(soup):
     img_src = soup.find('div', class_ = 'bookimage').find('img')['src']
     return urljoin('http://tululu.org', img_src)
 
@@ -53,7 +42,7 @@ def download_book(url_book,id):
     id_download = url_book[url_book.find('/b')+2:-1] #так как для закачки книги ссылка совсем другая нужен id
     url_download = f'http://tululu.org/txt.php?id={id_download}'
     response = requests.get(url_download, allow_redirects=False)
-    filename = f"{id+1}-я книга. {parsing_text(url_book)}.txt"
+    filename = f"{id+1}-я книга. {parsing_text(soup)}.txt"
     folder = os.path.join(PATCH_BOOKS, filename)
     with open(folder, "w", encoding='utf-8') as file:
         return file.write(response.text)
@@ -79,13 +68,15 @@ if __name__ == '__main__':
     list = []
     for id in range(10):
         url_book = urls[id]
-        url_img = parsing_image(url_book)
-        book_info = parsing_text(url_book).split(' -- ')
-        comments = parsing_comments(url_book)
-        genres = parsing_genres(url_book)
+        response = requests.get(url_book)
+        soup = BeautifulSoup(response.text, 'lxml')
+        url_img = parsing_image(soup)
+        book_info = parsing_text(soup).split(' -- ')
+        comments = parsing_comments(soup)
+        genres = parsing_genres(soup)
         url_src = os.path.join('images',url_img.split('/')[-1])
         book_path = os.path.join('books',book_info[0] + '.txt')
-        fantosy_book = {
+        book_info = {
             'title':book_info[0],
             "author": book_info[1],
             'img_src':url_src,
@@ -93,7 +84,7 @@ if __name__ == '__main__':
             'comments':comments,
             "genres": genres
         }
-        list.append(fantosy_book)
+        list.append(book_info)
         download_img(PATCH_IMG,url_img)
         download_book(url_book,id)
     with open("capitals.json", "a", encoding='utf-8') as my_file:
