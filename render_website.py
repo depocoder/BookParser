ad9f130ad66a5b7ce4679e9e68a421ea453c0a16
herypank <60302361+herypank@users.noreply.github.com>
2020-11-07 22:06:01 +0300

    Оптимизация кода.

--- render_website.py
+++ render_website.py
@@ -1,13 +1,11 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from livereload import Server, shell
-from more_itertools import chunked
 
 
 with open("about_books.json", "r") as my_file:
     about_books = my_file.read()
 about_books = json.loads(about_books)
-chunked_about_books = list(chunked(about_books, 2))
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -17,7 +15,7 @@ env = Environment(
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    chunked_about_books=chunked_about_books
+    about_books=about_books
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
